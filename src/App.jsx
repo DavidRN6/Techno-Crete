1ca659c755ad1c445fb223874ff33dabace6d52d
David <yugiohgxd600@gmail.com>
2025-03-22 10:44:31 -0700

    burger menu

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,12 @@ import { FaArrowUp } from "react-icons/fa6";
 import Projects from "./pages/Projects";
 import ProjectPage from "./pages/ProjectPage";
 
-//==================
-// Loading Screen
-//==================
-const LoadingScreen = () => (
-  <div className="flex items-center justify-center h-screen w-full bg-gray-100">
-    <div className="text-center">
-      <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-yellow-500"></div>
-      <p className="mt-4 text-gray-600 font-semibold">Loading...</p>
-    </div>
-  </div>
-);
-
 function App() {
-  const [loading, setLoading] = useState(true);
   const [scroll, setScroll] = useState(false);
 
-//==================
-// Scroll To Top
-//==================
+  //==================
+  // Scroll To Top
+  //==================
   useEffect(() => {
     const handleScroll = () => setScroll(window.scrollY > 200);
     window.addEventListener("scroll", handleScroll);
@@ -33,42 +20,30 @@ function App() {
     return () => window.removeEventListener("scroll", handleScroll); // تنظيف الحدث
   }, []);
 
-//=======================
-// Loading Screen Timer
-//=======================
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <div>
-      {loading ? (
-        <LoadingScreen />
-      ) : (
-        <div id="up">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/project/:productId" element={<ProjectPage />} /> 
-          </Routes>
-          <Footer />
+    <>
+      <div id="up">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:productId" element={<ProjectPage />} />
+        </Routes>
+        <Footer />
 
-          {/*==============
+        {/*==============
             Scroll Button
           =================*/}
-          <button
-            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            style={{ opacity: scroll ? 1 : 0, transition: "1s" }}
-            className="w-9 h-9 bg-yellow-500 text-white fixed bottom-8 right-[3%] 
+        <button
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          style={{ opacity: scroll ? 1 : 0, transition: "1s" }}
+          className="w-9 h-9 bg-yellow-500 text-white fixed bottom-8 right-[3%] 
                       border border-white/20 rounded-full p-3 transition-all duration-300"
-          >
-            <FaArrowUp className="w-full h-full" />
-          </button>
-        </div>
-      )}
-    </div>
+        >
+          <FaArrowUp className="w-full h-full" />
+        </button>
+      </div>
+    </>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import { BiMenuAltRight } from "react-icons/bi";
-import { IoClose } from "react-icons/io5";
 import { Link } from "react-router";
-import { motion, AnimatePresence } from "framer-motion";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,7 +8,7 @@ function Navbar() {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
     setIsMenuOpen(false);
   };
-/*==================
+  /*==================
   1. Menu Function
 ====================*/
 
@@ -25,91 +22,115 @@ function Navbar() {
       className="w-full fixed flex justify-between items-center bg-white text-gray-800
     px-4 py-3 lg:py-2 top-0 z-50 shadow-lg"
     >
-      <Link to="/" onClick={() => scrollToSection("home")} id="logo" className="md:text-2xl text-lg font-bold drop-shadow-[0_0_15px_rgba(234,179,8)]">
+      <Link
+        to="/"
+        onClick={() => scrollToSection("home")}
+        id="logo"
+        className="sm:text-2xl text-lg font-bold drop-shadow-[0_0_15px_rgba(234,179,8)]"
+      >
         TechnoCrete <span className="text-yellow-500">Constructions</span>
       </Link>
 
       <ul className="lg:flex justify-center items-center gap-6 hidden">
-        <Link to="/" onClick={() => scrollToSection("home")} className="font-bold cursor-pointer text-[15px] p-3 rounded-full hover:bg-yellow-500 hover:text-gray-800 transition-all duration-200">
+        <Link
+          to="/"
+          onClick={() => scrollToSection("home")}
+          className="font-bold cursor-pointer text-[15px] p-3 rounded-full hover:bg-yellow-500 hover:text-gray-800 transition-all duration-200"
+        >
           HOME
         </Link>
-        <Link to="/" onClick={() => scrollToSection("about")} className="font-bold cursor-pointer text-[15px] p-3 rounded-full hover:bg-yellow-500 hover:text-gray-800 transition-all duration-200">
+        <Link
+          to="/"
+          onClick={() => scrollToSection("about")}
+          className="font-bold cursor-pointer text-[15px] p-3 rounded-full hover:bg-yellow-500 hover:text-gray-800 transition-all duration-200"
+        >
           ABOUT
         </Link>
-        <Link to="/projects" className="font-bold cursor-pointer text-[15px] p-3 rounded-full hover:bg-yellow-500 hover:text-gray-800 transition-all duration-200">
+        <Link
+          to="/projects"
+          onClick={() => setIsMenuOpen(false)}
+          className="font-bold cursor-pointer text-[15px] p-3 rounded-full hover:bg-yellow-500 hover:text-gray-800 transition-all duration-200"
+        >
           PROJECTS
         </Link>
-        <Link to="/" onClick={() => scrollToSection("contact")} className="font-bold cursor-pointer text-[15px] p-3 rounded-full hover:bg-yellow-500 hover:text-gray-800 transition-all duration-200">
+        <Link
+          to="/"
+          onClick={() => scrollToSection("contact")}
+          className="font-bold cursor-pointer text-[15px] p-3 rounded-full hover:bg-yellow-500 hover:text-gray-800 transition-all duration-200"
+        >
           CONTACT US
         </Link>
       </ul>
 
       {/* ===========
-        Monile Menu
+        Mobile Menu
       ===============*/}
 
-      <div
-        className="flex justify-between items-center lg:hidden"
-        onClick={toggleMenu}
-      >
-        <div>
-          {isMenuOpen ? (
-            <IoClose className="text-yellow-500 text-3xl md:text-4xl cursor-pointer" />
-          ) : (
-            <BiMenuAltRight className="text-yellow-500 text-3xl md:text-4xl  cursor-pointer" />
-          )}
-        </div>
-      </div>
+      <div className="flex items-center justify-center p-[5px] lg:hidden">
+        {/* Burger Icon */}
+        <button
+          className="relative w-7 h-5 focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <span
+            className={`block absolute left-0 w-full h-[3px] bg-yellow-500 transition-all duration-500 rounded-sm ${
+              isMenuOpen ? "rotate-45 top-1/2 -translate-y-1/2" : "top-0"
+            }`}
+          ></span>
+          <span
+            className={`block absolute left-0 w-full h-[3px] bg-yellow-500 transition-all duration-500 rounded-sm ${
+              isMenuOpen ? "opacity-0" : "top-1/2 -translate-y-1/2"
+            }`}
+          ></span>
+          <span
+            className={`block absolute left-0 w-full h-[3px] bg-yellow-500 transition-all duration-500 rounded-sm ${
+              isMenuOpen ? "-rotate-45 top-1/2 -translate-y-1/2" : "bottom-0"
+            }`}
+          ></span>
+        </button>
 
-      <AnimatePresence>
+        {/* Menu Items */}
         {isMenuOpen && (
-          <motion.div
-            initial={{ opacity: 0, y: "-100%" }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: "-100%" }}
-            transition={{ duration: 0.4, ease: "easeInOut" }}
-            className="w-full bg-white p-4 absolute top-[45px] md:top-[51px] left-0 shadow-md"
-          >
-            <ul className="flex flex-col gap-2 w-full items-center justify-center">
-              <Link to="/" onClick={() => scrollToSection("home")} className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800">
-                HOME
-              </Link>
-              <Link to="/" onClick={() => scrollToSection("about")} className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800">
-                ABOUT
-              </Link>
-              <Link to="/projects" className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800">
-                PROJECTS
-              </Link>
-              <Link to="/" onClick={() => scrollToSection("contact")} className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800">
-                CONTACT US
-              </Link>
-            </ul>
-          </motion.div>
+                <div
+                className={`w-full bg-white p-4 absolute top-[45px] md:top-[55px] left-0 shadow-md  transition-all duration-500 ${
+                  isMenuOpen
+                    ? "opacity-100 translate-y-0"
+                    : "opacity-0 -translate-y-4"
+                }`}
+              >
+                <ul className="flex flex-col gap-2 w-full items-center justify-center">
+                  <Link
+                    to="/"
+                    onClick={() => scrollToSection("home")}
+                    className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800"
+                  >
+                    HOME
+                  </Link>
+                  <Link
+                    to="/"
+                    onClick={() => scrollToSection("about")}
+                    className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800"
+                  >
+                    ABOUT
+                  </Link>
+                  <Link
+                    to="/projects"
+                    className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800"
+                  >
+                    PROJECTS
+                  </Link>
+                  <Link
+                    to="/"
+                    onClick={() => scrollToSection("contact")}
+                    className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800"
+                  >
+                    CONTACT US
+                  </Link>
+                </ul>
+              </div>
         )}
-      </AnimatePresence>
-
-      {/* <div
-        className={`${
-          isMenuOpen ? "flex" : "hidden"
-        } w-full h-fit bg-white p-4 absolute
-      top-[45px] md:top-[51px] left-0 transition-all duration-300`}
-        onClick={closeMenu}
-      >
-        <ul className="flex flex-col gap-2 w-full items-center justify-center">
-          <Link to="/" onClick={() => scrollToSection("home")} className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800">
-            HOME
-          </Link>
-          <Link to="/" onClick={() => scrollToSection("about")} className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800">
-            ABOUT
-          </Link>
-          <Link to="/projects" className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800">
-            PROJECTS
-          </Link>
-          <Link to="/" onClick={() => scrollToSection("contact")} className="w-full text-center text-yellow-500 font-semibold cursor-pointer p-2 rounded-lg hover:bg-yellow-500 hover:text-gray-800">
-            CONTACT US
-          </Link>
-        </ul>
-      </div> */}
+  
+      </div>
     </nav>
   );
 }
